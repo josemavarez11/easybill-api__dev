d080e96f24ea25245fb3cf59ff3ccb7dfd41c80d
UldrenWEB <uldrenmiguel33@gmail.com>
2024-03-17 22:37:57 -0400

    Se agreraon en app.ts codigo para probar ts-node-dev

--- src/app.ts
+++ src/app.ts
@@ -5,4 +5,15 @@
  */
 
 import express from 'express';
-import dotenv from 'dotenv';
+import _dotenv from 'dotenv';
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use('/api', (_req, res) => {
+    res.send('Hello, World!');
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT} in http://localhost:${PORT}`);
+})
