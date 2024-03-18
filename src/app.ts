477f2559cb724f7ac3df06d7d9f6b25a4fbfe7b1
UldrenWEB <uldrenmiguel33@gmail.com>
2024-03-18 16:19:32 -0400

    Se agrego el generador de SKU para los productos y se instalo la libreria bcrypt y sus tipos con @types/bcrypt

--- src/app.ts
+++ src/app.ts
@@ -5,11 +5,11 @@
  */
 
 import ExpressAdapter from './adapters/expressAdapter';
-
 import corsMiddleware from './middlewares/corsMiddleware';
 import reqReceivedMiddleware from './middlewares/reqReceivedMiddleware';
 import indexRouter from './routing/indexRouter';
 import _mongoConnection from './db/mongoConnection';
+import skuGenerator from './utils/skuGenerator';
 
 
 const adapter = new ExpressAdapter({ portDefault: 3000 });
@@ -22,6 +22,14 @@ adapter.middlewarePersonalized({ middleware: reqReceivedMiddleware });
 
 adapter.setRouteApp({ route: '/api', callbackRouter: indexRouter() });
 
+const result = skuGenerator({
+    productId: '123',
+    productName: 'Balon',
+    category: 'Deportes'
+});
+
 adapter.startServer(() => {
+
+    console.log(result);
     console.log(`Server started at http://localhost:${adapter.port}`);
 });
